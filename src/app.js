11d207fe813e385c560c0dbbf69e6bd80713b264
Kůs Jiří <jiri.kus@ceskapojistovna.cz>
2017-11-30 21:11:32 +0100

    tic-tac-toe: todolist

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
+/**
+ * TODO: 
+1. Display the location for each move in the format (col, row) in the move history list.
+2. Bold the currently selected item in the move list.
+3. Rewrite Board to use two loops to make the squares instead of hardcoding them.
+4. Add a toggle button that lets you sort the moves in either ascending or descending order.
+5. When someone wins, highlight the three squares that caused the win.
+6. variable board size
+  - dynamic win codnition detection
+  - fields to create a board
+7. score sheet
+ - player names
+8. variable target length (3,5,7)
+9. tournament mode with spider
+10. online play with lobby :D
+ * 
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
