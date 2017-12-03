4a6170faa0cf6c08edbcb314defed35e07804fb1
Kůs Jiří <jiri.kus@ceskapojistovna.cz>
2017-12-03 14:19:01 +0100

    simple redux app

--- src/app.js
+++ src/app.js
@@ -1,27 +1,26 @@
-/**
- * TODO: 
-1. Display the location for each move in the format (col, row) in the move history list.
-2. Bold the currently selected item in the move list.
-3. Rewrite Board to use two loops to make the squares instead of hardcoding them.
-4. Add a toggle button that lets you sort the moves in either ascending or descending order.
-5. When someone wins, highlight the three squares that caused the win.
-6. variable board size
-  - dynamic win codnition detection
-  - fields to create a board
-7. score sheet
- - player names
-8. variable target length (3,5,7)
-9. tournament mode with spider
-10. online play with lobby :D
- * 
- */
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+let nextId = 0;
+
 export default class App extends React.Component {
   render() {
-    return <div></div>;
+    return (
+    <div>
+      <button onClick={()=>{
+          this.props.store.dispatch({
+            type: 'ADD_TODO',
+            text: 'test',
+            id: nextId++
+          })
+        }}>add</button>
+        <ul>
+          {this.props.todos.map(todo =>
+            <li key ={todo.id}>{todo.text}</li>
+          )}
+        </ul>
+    </div>
+    )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,63 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './app';
+import {combineReducers} from 'redux';
+import {createStore} from 'redux';
 
-render(
-<App/>
-       , document.getElementById('app'));
+const todo = (state, action) => {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      }
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state;
+      }
+      return {
+          ...state,
+          completed: !state.completed
+        }
+    default:
+      return state;
+  }
+}
+
+const todos = (state = [], action) => {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return [...state, todo(undefined, action)];
+    case 'TOGGLE_TODO':
+      return state.map(t => todo(t, action));
+    default:
+      return state;
+  }
+}
+
+const visibilityFilter = (
+  state = 'SHOW_ALL', 
+  action
+  ) => {
+    switch (action.type) {
+      case 'SET_VISIBILITY_FILTER':
+        return action.filter;
+      default: 
+        return state;  
+    }
+  } 
+
+const todoApp = combineReducers({
+  todos, 
+  visibilityFilter});
+const store = createStore(todoApp);
+
+const render = () => {
+    ReactDOM.render(<App todos= {store.getState().todos}
+                         store={store}/>, document.getElementById('app'));    
+}
+
+store.subscribe(render);
+render();
+
