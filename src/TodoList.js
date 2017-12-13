1346e3483911de6c411e29c2d3e68bdcf662d47f
Kůs Jiří <jiri.kus@ceskapojistovna.cz>
2017-12-13 21:42:25 +0100

    separate Todo and TodoList to separate files

---
+++ src/TodoList.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Todo from './Todo';
+
+
+const getVisibleTodos = (
+    todos,
+    filter
+  ) => {
+    switch (filter) {
+      case 'SHOW_ALL':
+        return todos;
+      case 'SHOW_COMPLETED':
+        return todos.filter(t => t.completed);
+      case 'SHOW_ACTIVE':
+        return todos.filter(t => !t.completed);
+    }
+  }
+
+const TodoList = ({
+    todos,
+    onTodoClick
+  }) => (
+      <ul>
+        {todos.map(todo =>
+          <Todo key={todo.id}
+            {...todo}
+            onClick={() => onTodoClick(todo.id)} />
+        )}
+      </ul>
+);
+
+const mapStateToProps = (state) => {
+    return {
+      todos: getVisibleTodos(
+        state.todos,
+        state.visibilityFilter)
+    };
+  }
+  
+const mapDispatchToProps = (dispatch) => {
+    return {
+      onTodoClick:  (id) => {dispatch({ type: 'TOGGLE_TODO', id })}
+    };
+  }
+
+  /**
+   * Note that connect is order dependant!!!!
+   * State = first argument
+   * Dispatch = second argument
+   */
+const VisibleTodoList = connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(TodoList);
+
+  export default VisibleTodoList;
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
+import VisibleTodoList from './Todolist'
 import './index.css';
 
 let nextId = 0;
@@ -51,65 +52,6 @@ class FilterLink extends React.Component {
 FilterLink.contextTypes = {
   store: React.PropTypes.object
 };
-
-const Todo = ({
-  onClick,
-  completed,
-  text
-}) => (
-    <li
-      onClick={onClick}
-      style={{
-        textDecoration: completed ? 'line-through' : 'none'
-      }}>{text}</li>
-  );
-
-const TodoList = ({
-  todos,
-  onTodoClick
-}) => (
-    <ul>
-      {todos.map(todo =>
-        <Todo key={todo.id}
-          {...todo}
-          onClick={() => onTodoClick(todo.id)} />
-      )}
-    </ul>
-  );
-
-
-  const getVisibleTodos = (
-    todos,
-    filter
-  ) => {
-    switch (filter) {
-      case 'SHOW_ALL':
-        return todos;
-      case 'SHOW_COMPLETED':
-        return todos.filter(t => t.completed);
-      case 'SHOW_ACTIVE':
-        return todos.filter(t => !t.completed);
-    }
-  }
-
-  const mapStateToProps = (state) => {
-    return {
-      todos: getVisibleTodos(
-        state.todos,
-        state.visibilityFilter)
-    };
-  }
-  
-  const mapDispatchToProps = (dispatch) => {
-    return {
-      onTodoClick:  (id) => {dispatch({ type: 'TOGGLE_TODO', id })}
-    };
-  }
-
-  const VisibleTodoList = connect(
-    mapDispatchToProps,
-    mapStateToProps
-  )(TodoList);
 
 const Footer = () => (
 
