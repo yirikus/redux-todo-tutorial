1eb69d3b35c4497a2ee22d20c170354a7cd26d75
Kůs Jiří <jiri.kus@ceskapojistovna.cz>
2017-12-13 22:32:49 +0100

    redux tutorial step 29: connect FilterLink

--- src/FilterLink.js
+++ src/FilterLink.js
@@ -2,29 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Link from './Link';
 
-class FilterLink extends React.Component {
-    componentDidMount() {
-      this.unsubscribe = this.context.store.subscribe(() => this.forceUpdate());
-    }
-  
-    componentWillUnmount() {
-      this.unsubscribe();
-    }
-  
-    render() {
-      const props = this.props;
-      const {store} = this.context;
-      const state = store.getState();
-  
-      const onFilterClick = () =>
-        store.dispatch({ type: 'SET_VISIBILITY_FILTER', filter: props.filter });
-  
-      return <Link active={props.filter === state.visibilityFilter}
-        onClick={onFilterClick}>{props.children}</Link>
-    }
+
+
+const mapStateToProps = (state, props) => {
+    return {
+      active: props.filter === state.visibilityFilter
+    };
   }
-  FilterLink.contextTypes = {
-    store: React.PropTypes.object
-  };
+  
+const mapDispatchToProps = (dispatch, props) => {
+    return {
+      onClick:  () => {
+          dispatch({ 
+              type: 'SET_VISIBILITY_FILTER', 
+              filter: props.filter });
+            }
+    };
+  }
+
+  const FilterLink = connect(
+      mapStateToProps, 
+      mapDispatchToProps)(Link); 
 
   export default FilterLink;
